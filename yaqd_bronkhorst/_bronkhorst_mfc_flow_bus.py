da7f2e6bc706025e1a8c158c70340f13272dbae2
Blaise Thompson <blaise@untzag.com>
2022-12-13 13:50:12 -0600

    minimum working with hardware (#1)
    * minimum working with hardware
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * type ignore
    
    * type ignore propar
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- yaqd_bronkhorst/_bronkhorst_mfc_flow_bus.py
+++ yaqd_bronkhorst/_bronkhorst_mfc_flow_bus.py
@@ -3,24 +3,53 @@ __all__ = ["BronkhorstMfcFlowBus"]
 import asyncio
 from typing import Dict, Any, List
 
-from yaqd_core import IsDaemon
+import propar  # type: ignore
+from yaqd_core import (
+    HasTransformedPosition,
+    HasLimits,
+    HasPosition,
+    UsesSerial,
+    UsesUart,
+    IsDaemon,
+)
 
 
-class BronkhorstMfcFlowBus(IsDaemon):
+class BronkhorstMfcFlowBus(
+    HasTransformedPosition, HasLimits, HasPosition, UsesUart, UsesSerial, IsDaemon
+):
     _kind = "bronkhorst-mfc-flow-bus"
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
-        # Perform any unique initialization
+        self._instrument = propar.instrument(
+            comport=self._config["serial_port"], baudrate=self._config["baud_rate"]
+        )
+        if self._instrument.setpoint is not None:
+            self._state["destination"] = self._instrument.setpoint
+
+    def direct_serial_write(self, _bytes):
+        self._instrument.master.propar.serial.write(_bytes)
+
+    def get_temperature(self) -> float:
+        return self._instrument.readParameter(142)
+
+    def _relative_to_transformed(self, relative_position):
+        xp = [p["setpoint"] for p in self._config["calibration"]]
+        fp = [p["measured"] for p in self._config["calibration"]]
+        out = np.interp(relative_position, xp, fp)
+        return out
+
+    def _set_position(self, position):
+        self._instrument.setpoint = position
+
+    def _transformed_to_relative(self, transformed_position):
+        xp = [p["measured"] for p in self._config["calibration"]]
+        fp = [p["setpoint"] for p in self._config["calibration"]]
+        return np.interp(transformed_position, xp, fp)
 
     async def update_state(self):
-        """Continually monitor and update the current daemon state."""
-        # If there is no state to monitor continuously, delete this function
         while True:
-            # Perform any updates to internal state
-            self._busy = False
-            # There must be at least one `await` in this loop
-            # This one waits for something to trigger the "busy" state
-            # (Setting `self._busy = True)
-            # Otherwise, you can simply `await asyncio.sleep(0.01)`
-            await self._busy_sig.wait()
+            self._state["position"] = self._instrument.measure
+            if abs(self._state["position"] - self._state["destination"]) < 1.0:
+                self._busy = False
+            await asyncio.sleep(0.25)
