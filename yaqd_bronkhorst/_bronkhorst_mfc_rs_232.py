2c20651d175aa5071ecc2bad5053c5c711705bf1
Unni Kurumbail <100881139+ukurumbail@users.noreply.github.com>
2023-08-18 13:36:25 -0500

    yaqd-bronkhorst-mfc-rs-232 (#17)
    * Develops a working version of a yaq daemon for bronkhorst rs-232 communications with one MFC.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * mypy
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Blaise Thompson <blaise@untzag.com>

---
+++ yaqd_bronkhorst/_bronkhorst_mfc_rs_232.py
@@ -0,0 +1,76 @@
+__all__ = ["BronkhorstMfcRS232"]
+
+import asyncio
+from typing import Dict, Any, List
+import numpy as np
+import serial  # type: ignore
+
+
+from yaqd_core import (
+    HasTransformedPosition,
+    HasLimits,
+    HasPosition,
+    UsesSerial,
+    UsesUart,
+    IsDaemon,
+)
+
+
+class BronkhorstMfcRS232(
+    HasTransformedPosition, HasLimits, HasPosition, UsesUart, UsesSerial, IsDaemon
+):
+    _kind = "bronkhorst-mfc-rs-232"
+
+    def __init__(self, name, config, config_filepath):
+        super().__init__(name, config, config_filepath)
+        self._ser = serial.Serial(
+            self._config["serial_port"], baudrate=self._config["baud_rate"], timeout=0.5
+        )
+        if self._config["make"] is None:
+            self.make = "Bronkhorst"
+
+    def direct_serial_write(self, _bytes):
+        raise NotImplementedError
+
+    def _relative_to_transformed(self, relative_position):
+        xp = [p["setpoint"] for p in self._config["calibration"]]
+        fp = [p["measured"] for p in self._config["calibration"]]
+        out = np.interp(relative_position, xp, fp)
+        return out
+
+    def _set_position(self, position):
+        if position > 0:
+            position_str = (position / float(self._config["max_position"])) * 32000
+            position_str = hex(int(position_str))[2:]
+        else:
+            position_str = "0000"
+        position_str = ":0680010121" + position_str + "\r\n"
+        print(
+            "pos,pos_str,max_pos: {}{}{}".format(
+                position, position_str, self._config["max_position"]
+            )
+        )
+        self._ser.write(position_str.encode())
+
+    def _transformed_to_relative(self, transformed_position):
+        xp = [p["measured"] for p in self._config["calibration"]]
+        fp = [p["setpoint"] for p in self._config["calibration"]]
+        return np.interp(transformed_position, xp, fp)
+
+    async def update_state(self):
+        while True:
+            read_code = ":06800401210120\r\n"
+            self._ser.write(read_code.encode())
+            await asyncio.sleep(0.25)
+            val = self._ser.readline()
+
+            val = val.decode().rstrip()[-4:]  # Gets last 4 hex digits
+            num = int(val, 16)  # Converts to decimal
+            position = (float(num) / 32000) * float(
+                self._config["max_position"]
+            )  # Determines actual flow
+
+            self._state["position"] = position
+            if abs(self._state["position"] - self._state["destination"]) < 1.0:
+                self._busy = False
+            await asyncio.sleep(0.25)
